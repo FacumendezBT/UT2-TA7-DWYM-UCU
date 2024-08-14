5d630477f079ef4671dc8dc1f85ba61bb586c94a
Facundo Méndez Bentos <facumendezbt@gmail.com>
2024-08-14 10:14:39 -0300

    UT2-TA7

---
+++ script.js
@@ -0,0 +1,37 @@
+const btn = document.getElementById('btn');
+const books = [
+    {
+        title: 'Book',
+        author: 'Name'
+    },
+    {
+        title: 'Book2',
+        author: 'Name2'
+    },
+    {
+        title: 'Book3',
+        author: 'Name3'
+    },
+    {
+        title: 'Book4',
+        author: 'Name4'
+    },
+    {
+        title: 'Book5',
+        author: 'Name5'
+    },
+]
+
+btn.addEventListener('click', function () {
+    getTheTitles(books);
+});
+
+/**
+ * Función que devuele e imprime en consola un nuevo arreglo que contenga sólamente los títulos de los libros guardados en el parámetro books.
+ * @param {Array} books - Arreglo de objetos que contiene los libros.
+ */
+function getTheTitles(books) {
+    const titles = books.map(book => book.title);
+    console.log(titles);
+    return titles;
+}
